45ce225f041b8c223a0db10167b224485c471563
alan <cvinny33@gmail.com>
2022-08-19 17:25:57 -0600

    added readme text

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,7 +10,54 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
 
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Badges](#badges)
+  - [Features](#features)
+  - [How to Contribute](#how_to_Contribute)
+  - [Tests](#tests)
+
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage
+  
+  Provide instructions and examples for use. Include screenshots as needed.
+  
+  ## Credits
+  
+  List your collaborators, if any, with links to their GitHub profiles.
+  
+  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+  
+  If you followed tutorials, include links to those here as well.
+  
+  ## License
+  
+  ${data.license}
+  
+  ## Badges
+  
+  ![](https://img.shields.io/badge/license-${data.license}-blue)
+  
+  ## Features
+  
+  If your project has a lot of features, list them here.
+  
+  ## How to Contribute
+  
+  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+  
+  ## Tests
+  
 `;
 }
 
