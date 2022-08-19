cb2a05685b10415bea800bb7f7fbe9a8bd9ed1d4
alan <cvinny33@gmail.com>
2022-08-19 17:25:04 -0600

    added questions

--- index.js
+++ index.js
@@ -2,55 +2,56 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateReadme = ({})
-
-
-
-
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name:
-            message
-          },
-          {
-            type: 'input',
-            name: 
-            message
-          },
-          {
-            type: 'input',
-            name:
-            message
-          },
-          {
-            type: 'input',
-            name: 
-            message
-          },
-          {
-            type: 'input',
-            name:
-            message
-          },
-          {
-            type: 'input',
-            name:
-            message
-          },
-    ])
-
-
-
+const generateReadme = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: "title",
+    message: "What's the title of your repository"
+},
+// {
+//     type: 'input',
+//     name: "installation"
+//     message:
+// },
+// {
+//     type: 'input',
+//     name:
+//         message
+// },
+// {
+//     type: 'input',
+//     name:
+//         message
+// },
+// {
+//     type: 'input',
+//     name:
+//         message
+// },
+// {
+//     type: 'input',
+//     name:
+//         message
+// }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    fs.writeFile(`README.md`, data, (err) =>
+        err ? console.err(err) : console.log("Success!"));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+        const answers = await inquirer.prompt(questions)
+        const genStructure = generateReadme(answers);
+        writeToFile(genStructure);
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 // Function call to initialize app
 init();
